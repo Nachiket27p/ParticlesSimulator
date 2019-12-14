6e4135779edc98709edc6c0f9421034137ff5838
Nachiket27p <nachiket27p@gmail.com>
2019-12-14 14:32:34 -0500

    Moced around dependencies and added support to compile for x86 architecture

--- ParticlesSim-Engine/src/macros/IOHandler.h
+++ ParticlesSim-Engine/src/macros/IOHandler.h
@@ -7,3 +7,5 @@
 #define PRINT_GL_VERSION(msg) {std::cout << "OpenGL Version: " << (msg) << std::endl;}
 
 #define PRINT_WINDOW_SIZE(w,h) {std::cout << (w) << " x " << (h) << std::endl;}
+
+#define PRINT_MESSAGE(msg) {std::cerr << (msg) << std::endl;}
